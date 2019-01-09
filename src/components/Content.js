564ec1afe5ed5318aa04dd81bb86d8c0cc787259
krittiyaclark <krittiyaclark@gmail.com>
2019-01-09 16:33:36 -0500

    Add Header, Content and Footer components

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div>
+      <p>Content</p>
+    </div>
+  )
+}
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  )
+}
+export default Header;
