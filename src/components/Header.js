621afd1b4c60fdaa2d20e117aae60946e5a329da
krittiyaclark <krittiyaclark@gmail.com>
2019-01-10 18:26:28 -0500

    Install Gulp and add a gulpfile.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,16 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header className="app-header">
+      <h1>Hello, I'm Krittiya!</h1>
+      <p>Front End Web Developer</p>
+
+      <nav>home</nav>
+      <nav>about</nav>
+      <nav>portfolio</nav>
+      <nav>contact</nav>
+    </header>
+
   )
 }
 export default Header;
