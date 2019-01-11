e9e72d63e78a6d4621d3f0e1d54fc83194ea0166
krittiyaclark <krittiyaclark@gmail.com>
2019-01-10 22:44:26 -0500

    Change gulp version to 3.9.1 and solve a gulpfile.js file error

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Dependencies
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+
+// SCSS/CSS
+var SCSS_SRC = './src/Assets/scss/**/*.scss';
+var SCSS_DEST = './src/Assets/css';
+
+//Complie SCSS
+gulp.task('complie_scss', function() {
+  gulp.src(SCSS_SRC)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(changed(SCSS_DEST))
+    .pipe(gulp.dest(SCSS_DEST));
+})
+
+// Detect changes in SCSS
+gulp.task('watch_scss', function() {
+  gulp.watch(SCSS_SRC, ['compile_scss']);
+})
+
+// Run Tasks
+gulp.task('default', ['watch_scss']);
