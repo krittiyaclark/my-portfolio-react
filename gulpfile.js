e6f8a27b0b9f84b09f5aff376077bf7f56924af4
krittiyaclark <krittiyaclark@gmail.com>
2019-01-10 23:20:57 -0500

    Movethe gulpfile.js file

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ var rename = require('gulp-rename');
 var changed = require('gulp-changed');
 
 // SCSS/CSS
-var SCSS_SRC = './scr/Assets/scss/**/*.scss';
-var SCSS_DEST = './scr/Assets/css';
+var SCSS_SRC = './src/Assets/scss/**/*.scss';
+var SCSS_DEST = './src/Assets/css';
 
 //Complie SCSS
 gulp.task('complie_scss', function() {
