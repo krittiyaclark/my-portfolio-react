278d18289ff6b106fb5bb5fcd664c2f5c12d90a3
krittiyaclark <krittiyaclark@gmail.com>
2019-01-10 23:44:01 -0500

    Clean up unuse files

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,24 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
 
+
 // SCSS/CSS
 var SCSS_SRC = './src/Assets/scss/**/*.scss';
 var SCSS_DEST = './src/Assets/css';
 
-//Complie SCSS
-gulp.task('complie_scss', function() {
-  gulp.src(SCSS_SRC)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minifyCSS())
-    .pipe(rename({ suffix: '.min'}))
-    .pipe(changed(SCSS_DEST))
-    .pipe(gulp.dest(SCSS_DEST));
+// Compile SCSS
+gulp.task('compile_scss', function() {
+	gulp.src(SCSS_SRC)
+	  .pipe(sass().on('error', sass.logError))
+	  .pipe(minifyCSS())
+	  .pipe(rename({ suffix: '.min'}))
+	  .pipe(changed(SCSS_DEST))
+	  .pipe(gulp.dest(SCSS_DEST));
 })
 
 // Detect changes in SCSS
 gulp.task('watch_scss', function() {
-  gulp.watch(SCSS_SRC, ['compile_scss']);
+	gulp.watch(SCSS_SRC, ['compile_scss']);
 })
 
 // Run Tasks
