fd269a5bb65ac0e6af668047fb6116de6c06c8dd
krittiyaclark <krittiyaclark@gmail.com>
2019-01-20 21:51:04 -0500

    Updated color scheme

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -4,7 +4,7 @@ class Projects extends Component {
   render() {
     return (
       <div>
-        <section className='section text-center'>
+        <section className='section'>
           <div className='row'>
             <div className='col-sm'>
               <div className='content-left'>
@@ -12,8 +12,9 @@ class Projects extends Component {
               </div>
             </div>
             <div className='col-sm-8'>
-              <div className='content-right'>
-                <p>Front-End Web Developer Projects</p>
+              <div className='content-right text-left'>
+                <h1>Neighborhood Map</h1>
+                <p>This project creates the map of restaurants in New York City neighborhood for the Front End Web Developer Nanodegree for Udacity.</p>
               </div>
             </div>
           </div>
