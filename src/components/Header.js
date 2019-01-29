9189a577a667b7448b79c5af3fcb32fd323dcf63
krittiyaclark <krittiyaclark@gmail.com>
2019-01-29 14:49:25 -0500

    Responsive navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,30 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
   render() {
     return (
-      <header className='container-fluid'>
-        <div className='logo float-left'>
-          <Link to='/'><img src='#' />LOGO</Link>
+      <div>
+        <nav className='navbar navbar-expand-sm navbar-default navbar-fixed-top navbar-dark bg-dark'>
+          <Link to='/' className='navbar-brand'><img src='#' />LOGO</Link>
+            <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarText' aria-controls='navbarText' aria-expanded='false' aria-label='Toggle navigation'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarText'>
+              <ul className='navbar-nav ml-auto'>
+                <li className='nav-item'>
+                  <Link to='/About' className='nav-link'>about</Link>
+                </li>
+                <li>
+                  <Link to='/Projects' className='nav-link'>projects</Link>
+                </li>
+                <li>
+                  <Link to='/Contact' className='nav-link'>contact</Link>
+                </li>
+                <li>
+                  <Link to='/Resume' className='nav-link'>resume</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
-
-        <nav className='float-right'>
-          <ul>
-            <li>
-              <Link to='/About'>about</Link>
-            </li>
-            <li>
-              <Link to='/Projects'>projects</Link>
-            </li>
-            <li>
-              <Link to='/Contact'>contact</Link>
-            </li>
-            <li>
-              <Link to='/Resume'>resume</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
     )
   }
 }
