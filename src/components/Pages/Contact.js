98a9b819a8dec6277ff1917da85ad913114355aa
krittiyaclark <krittiyaclark@gmail.com>
2019-02-18 15:06:46 -0500

    Change the contact form layout

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -21,20 +21,33 @@ class Contact extends Component {
               <div className='col-sm-6 text-left'>
                   <h1 className='text-left'>Contact</h1>
 
-                    <div className="App">
+                    <div className='App'>
                         <div>
-                          <form action="/action_page.php">
-                            <label>First Name</label>
-                            <input type="text"id="fname"name="firstname"placeholder="Your name.."/>
-                            <label>Last Name</label>
-                            <input type="text"id="lname" name="lastname" placeholder="Your last name.."/>
+                          <form action='/action_page.php'>
+                            <div className='form__row'>
+                              <label className='form__label'>First Name</label>
+                              <input type='text'id='fname'name='firstname'placeholder='Your name..'/>
+                            </div>
 
-                            <label>Email</label>
-                            <input type="email"id="email"name="email"placeholder="Your email"/>
+                            <div className='form__row'>
+                              <label className='form__label'>Last Name</label>
+                              <input type='text' id='lname' name='lastname' placeholder='Your last name..'/>
+                            </div>
 
-                            <label>Subject</label>
-                            <textarea id="subject"name="subject"placeholder="Write something.."></textarea>
-                            <input type="submit"value="Submit"/>
+                            <div className='form__row'>
+                              <label className='form__label'>Email</label>
+                              <input type='email' id='email' name='email' placeholder='Your email'/>
+                            </div>
+
+                            <div className='form__row'>
+                              <label className='form__label'>Message</label>
+                              <textarea id='subject' name='message' placeholder='Write something..'></textarea>
+                            </div>
+
+                            <div className='form__row form__row--buttons'>
+                              <input type='submit' value='Submit'/>
+                            </div>
+
                           </form>
                         </div>
                     </div>
