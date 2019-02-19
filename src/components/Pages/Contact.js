8b476920c98dfe85bd95488d4e3255f992902fc7
krittiyaclark <krittiyaclark@gmail.com>
2019-02-18 22:35:30 -0500

    Adjust contact form

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -38,7 +38,7 @@ handleFormSubmit = e => {
       <section className='container-fluid text-center contact-body'>
           <div className='container hori-centered contact-grid'>
             <div className='row'>
-              <div className='col-sm-6 text-left'>
+              <div className='col-sm-8 text-left'>
                   <h1 className='text-left'>Contact</h1>
 
                     <div className='App'>
@@ -91,7 +91,7 @@ handleFormSubmit = e => {
                         </div>
                     </div>
               </div>
-              <div className='col-sm-6 text-left'>
+              <div className='col-sm-4 text-left'>
                 <h1 className='text-left'>Let's talk!</h1>
 
                 <p>You have front-end projects, great! I am excited to work with you. Please send me an email and tell me about the project detail. I usually reply email within 1 to 2 days.</p>
