bc3c515829224b566dbab8fdba76bcb9fbfde84b
krittiyaclark <krittiyaclark@gmail.com>
2019-06-25 11:11:36 -0400

    Updated resume

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -20,26 +20,39 @@ class Resume extends Component {
                   <div className="col-sm-4">
                       <h2>Technologies</h2>
                   </div>
-                  <div className="col-sm-8">
+                  <div className="col-sm-4">
                       <ul>
-                        <li>Sass / LESS</li>
+                        <li>Sass</li>
                         <li>HTML5 / CSS3</li>
                         <li>JavaScript / jQuery</li>
                         <li>React.js</li>
                         <li>React Native</li>
                         <li>CSS Flexbox</li>
-                        <li>Foundation / Bootstrap</li>
+                        <li>Foundation / Bootstrap 4</li>
+                        <li>Scalable Vector Graphics</li>
                         <li>CMS</li>
                         <li>WordPress</li>
+                        <li>UI Widgets</li>
                         <li>Google App Script</li>
-                        <li>Git</li>
-                        <li>GitHub</li>
+                        <li>Git/GitHub</li>
+                        <li>Command Line</li>
+                      </ul>
+                  </div>
+                  <div className="col-sm-4">
+                  <ul>
                         <li>Velocity</li>
+                        <li>Data Formats (JSON, XML)</li>
                         <li>Photoshop</li>
                         <li>Illustrator</li>
+                        <li>HTML Semantics</li>
+                        <li>Gulp</li>
                         <li>Responsive Layout and Design</li>
                         <li>Cross-Browser Compatibility</li>
+                        <li>Browser Developer Tools</li>
                         <li>Mobile First Thinking</li>
+                        <li>Search Engine Optimization</li>
+                        <li>Accessibility</li>
+                        <li>User Interface Design</li>
                       </ul>
                   </div>
                 </div>
@@ -54,34 +67,32 @@ class Resume extends Component {
                       <h2>Work Experience</h2>
                   </div>
                   <div className="col-sm-8">
-                    <h3>Web Developer, Contract</h3>
+                    <h3>Web Developer/Remote, Contract</h3>
                     <p><strong>Vermont Digger</strong>, February 2019 – Present</p>
                     <ul>
-                      <li>Troubleshoot, resolve, build applications, manipulate and implementation WordPress Web Platform</li>
+                      <li>Troubleshoot, resolve, build applications, optimization page-speed, create vector graphic SVG, build UI, manipulate and implementation WordPress Web Platform</li>
                       <li>Write Google app scripts to manipulate Mailchimp API</li>
-                      <li>Tools used include Atom, Coda, Trello and Google Docs</li>
-                      <li>Languages: HTML, CSS, Bootstrap, jQuery, JavaScript, Google App Script</li>
-                      <li>Collaborate remotely with another peer via Slack</li>
                     </ul>
 
                       <h3>Web Developer, Contract</h3>
                       <p><strong>Dealer.com/Cox Automotive Group</strong>, August 2018 – December 2018</p>
                       <ul>
-                        <li>Troubleshoot, resolve and implementation CMS Web Platform</li>
-                        <li>Perform quality assurance to ensure work is performed per specifications and meets both functional and aesthetic standards</li>
-                        <li>Tools used include Atom, Git</li>
-                        <li>Languages: HTML, CSS, XML, jQuery, JavaScript, Velocity</li>
-                        <li>Collaborate remotely with other peers via Slack and Skype</li>
+                        <li>Troubleshoot, resolved and implemented CMS Web Platform</li>
+                        <li>Performed quality assurance to ensure work is performed per specifications and met both functional and aesthetic standards</li>
                         <li>Average closed completed 130 tickets per month</li>
                       </ul>
 
-                      <h3>Facilitator, Remote</h3>
+                      <h3>Facilitator/Remote</h3>
                       <p><strong>Learn-To-Code</strong>, April 2018 – October 2018</p>
                       <p>I help facilitate collaboration at Learn-To-Code, a community-powered (coding). We are motivated individuals with shared goals and group them intentionally. The mission is to learn, help and build a project.</p>
 
-                      <h3>Front-End Web Developer, Author</h3>
+                      <h3>Front-End Web Developer/Remote, Author</h3>
                       <p><strong>Themeforest.net</strong>, January 2012 – December 2013</p>
-                      <p>Design web template to created mockups and translated the design to web templates from scratch and deployed websites to a web server using HTML5, CSS3, Bootstrap, JavaScript, and Adobe Illustrator.</p>
+                      <ul>
+                        <li>Designed web templates, created mock ups and translated the design to code using HTML5, CSS3, Bootstrap, JavaScript, and Adobe Illustrator</li>
+                        <li>Wrote the documentation</li>
+                        <li>Responsible for customer services</li>
+                      </ul>
 
                       <h3>Front-End Web Developer, Freelance</h3>
                       <p>January 2012 – April 2013</p>
@@ -110,55 +121,18 @@ class Resume extends Component {
                       <h2>Education</h2>
                   </div>
                   <div className="col-sm-8">
-                      <h3>Front-End Web Developer Nanodegree Program, Certification</h3>
-                      <p><strong>Udacity</strong>, January 2018 – October 2018</p>
+                      <h3>Udacity in Front-End Web Developer Nanodegree Program, Certification</h3>
+                      <p><strong>Remote</strong>, Jan 2018 – Oct 2018</p>
                       <p>I was awarded a GrowWithGoogle scholarship for Phase 1. The program included HTML, CSS, and JavaScript fundamental. After I finished the Phase 1, I was the top 10% of students who were awarded Phase 2 scholarships.</p>
 
                       <p>Front-end Web Developer Naodregree Phase 2 covered: HTML, CSS, responsive website design, accessibilities, API, Gulp, jQuery, Jasmine, React and Service Workers. Built 7 projects that are covered all the technologies that mentioned.</p>
 
-                      <h3>Fine and Applied Art, Bachelor of Arts, BA</h3>
-                      <p><strong>Suan Sunandha Rajabhat University</strong>, April 1999 – April 2003</p>
-                      <p>Fine and Applied Art is a concentration within the visual arts field that includes the areas of advertising, commercial design, commercial photography, graphic design, illustration, and drawing.</p>
-                  </div>
-                </div>
-             </div>
-          </section>
+                      <h3>Community College of Vermont in Web Design, Certificates</h3>
+                      <p><strong>Winooski VT, USA,</strong> Apr, 2010 - Dec, 2011</p>
 
-          {/* Projects */}
-          <section className="container-fluid Top-padding">
-              <div className="container text-left">
-                <div className="row">
-                  <div className="col-sm-4">
-                      <h2>Projects</h2>
-                  </div>
-                  <div className="col-sm-8">
-                      <ul>
-                        <li>
-                          <strong>Neighborhood Map</strong> HTML, CSS, React, Service Worker, Accessibility, Responsive design
-                          <br />
-                          <a className="redirectLink" onClick={()=>this.setRedirect("https://krittiyaclark.github.io/udacity-front-end-neighborhood-map-react/")}>Project link</a>
-                        </li>
-                        <li>
-                          <strong>Bookshelf</strong> HTML, CSS, React
-                          <br />
-                          <a className="redirectLink" onClick={()=>this.setRedirect("https://krittiyaclark.github.io/udacity-front-end-React-MyReads-Project/#/")}>Project link</a>
-                        </li>
-                        <li>
-                          <strong>Memory Game</strong> HTML, CSS, Bootstrap, JavaScript, Responsive design
-                          <br />
-                          <a className="redirectLink" onClick={()=>this.setRedirect("http://threatening-mass.surge.sh/")}>Project link</a>
-                        </li>
-                        <li>
-                          <strong>Simple</strong> HTML, CSS, React, Service Worker, Accessibility, Responsive design
-                          <br />
-                          <a className="redirectLink" onClick={()=>this.setRedirect("http://striped-dolls.surge.sh/")}>Project link</a>
-                        </li>
-                        <li>
-                          <strong>Momentumdash</strong> HTML, CSS, JavaScript, JSON
-                          <br />
-                          <a className="redirectLink" onClick={()=>this.setRedirect("https://krittiyaclark.github.io/Momentumdash/")}>Project link</a>
-                        </li>
-                      </ul>
+                      <h3>Suan Sunandha Rajabhat University in Fine and Applied Art, Bachelor of Arts, BA</h3>
+                      <p><strong>Bangkok, Thailand</strong>, Apr 1999 – Apr 2003</p>
+                      <p>Fine and Applied Art is a concentration within the visual arts field that includes the areas of advertising, commercial design, commercial photography, graphic design, illustration, and drawing.</p>
                   </div>
                 </div>
              </div>
