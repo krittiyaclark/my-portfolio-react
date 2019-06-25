53cb954678005f1f0094eb7fd37d041d0767d56e
krittiyaclark <krittiyaclark@gmail.com>
2019-06-25 00:09:43 -0400

    Resolve image paths

--- src/components/ProjectLists.js
+++ src/components/ProjectLists.js
@@ -1,7 +1,7 @@
 const ProjectLists = [
     {
         id: "1",
-        iconImage: "img/neighborhood-map-react.jpg",
+        iconImage: "/my-portfolio-react/img/neighborhood-map-react.jpg",
         name: "Neighborhood Map",
         description: "This project creates the map of restaurants in New York City neighborhood for the Front End Web Developer Nanodegree for Udacity.",
         technologies: "HTML, CSS, Resposive Website Design, Accessibility, Service Worker, React",
@@ -9,7 +9,7 @@ const ProjectLists = [
     },
     {
         id: "2",
-        iconImage: "img/bookshelf-react.jpg",
+        iconImage: "/my-portfolio-react/img/bookshelf-react.jpg",
         name: "Bookshelf",
         description: "This project creates a library of books for Front End Nanodegree for Udacity.",
         technologies: "HTML, CSS, Resposive Website Design, Accessibility, React",
@@ -17,7 +17,7 @@ const ProjectLists = [
     },
     {
         id: "3",
-        iconImage: "img/memory-game.jpg",
+        iconImage: "/my-portfolio-react/img/memory-game.jpg",
         name: "Memory Game",
         description: "This is a game built to test a user memory. There's a deck of cards with different icons which the user should matched them.",
         technologies: "HTML, CSS, JavaScript",
@@ -25,7 +25,7 @@ const ProjectLists = [
     },
     {
         id: "4",
-        iconImage: "img/tic-tac-toe.jpg",
+        iconImage: "/my-portfolio-react/img/tic-tac-toe.jpg",
         name: "Tic-Tac-Toe",
         description: "Either a user or a computer will win the game by two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.",
         technologies: "HTML, CSS, JavaScript",
@@ -33,7 +33,7 @@ const ProjectLists = [
     },
     {
         id: "5",
-        iconImage: "img/responsive-website.jpg",
+        iconImage: "/my-portfolio-react/img/responsive-website.jpg",
         name: "Simple",
         description: "Simple is a corporate responsive template and the absolutely clean & modern template theme for your business. The theme was built by Foundation Framework and take advantages of it features: grid system, typography, buttons, form, UI element, section and more.",
         technologies: "HTML, CSS, JavaScript",
@@ -41,7 +41,7 @@ const ProjectLists = [
     },
     {
         id: "6",
-        iconImage: "img/momentum.png",
+        iconImage: "/my-portfolio-react/img/momentum.png",
         name: "Momentum clone",
         description: "This web app is a clone of Momentum Dash, a popular Chrome extension that replaces the new tab page. This web app is a personal homepage, or dashboard, that will help you be more productive by providing a to-do list and a daily focus reminder. It also displays the time, weather, inspirational quotes, and random photos.",
         technologies: "HTML, CSS, JavaScript",
@@ -49,7 +49,7 @@ const ProjectLists = [
     },
     {
         id: "7",
-        iconImage: "img/ux_ui_credentials.jpg",
+        iconImage: "/my-portfolio-react/img/ux_ui_credentials.jpg",
         name: "Login form",
         description: "The login form is a UX & UI desing built with React, SASS and Bootstrap. It applied CSS animation for the button effect.",
         technologies: "HTML, CSS, React, SASS and Bootstrap",
