e71d9c96f200a7d6360e7757795a8def38dd0b50
krittiyaclark <krittiyaclark@gmail.com>
2019-06-25 12:13:26 -0400

    Updates

---
+++ precache-manifest.6113e7eb729375ab2859bd677f03d5b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43110439f65ff844f42e",
+    "url": "/krittiyaclark/my-portfolio-react/static/css/main.985d17af.chunk.css"
+  },
+  {
+    "revision": "43110439f65ff844f42e",
+    "url": "/krittiyaclark/my-portfolio-react/static/js/main.43110439.chunk.js"
+  },
+  {
+    "revision": "df116914106a3d053252",
+    "url": "/krittiyaclark/my-portfolio-react/static/js/1.df116914.chunk.js"
+  },
+  {
+    "revision": "2753615a92452a566106",
+    "url": "/krittiyaclark/my-portfolio-react/static/js/runtime~main.2753615a.js"
+  },
+  {
+    "revision": "744ec351560461276aedee3187f95021",
+    "url": "/krittiyaclark/my-portfolio-react/index.html"
+  }
+];
